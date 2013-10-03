b305637cc89cfc201cb473bbfa1ea8f578186add
dvdekow <dvdekow@gmail.com>
2013-10-03 14:22:37 +0700

    modeling graph

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -19,13 +19,38 @@ class ResultsController < ApplicationController
 	def index
 	 @results = Result.new
 	 @branch = Branch.new
-	end
-
-	def generate
-
+	 @neo = Neography::Rest.new
 	end
 
 	def makeRelations
+    	/check is user_node exist?/
+		  /if not_exist? create new user_node
+           check jika telah melihat item lain
+             jika ada maka buat node melihat yang lain
+           check jika telah membeli barang lain
+             jika ada maka buat node beli yang lain
+           check jika pernah memberi rating
+             jika pernah memberi rating pada barang
+           check apakah ia penjual barang
+             jika pernah check rekomendasinya
+           
+		  	/
+			/check is item_node exist?/
+              /if not_exist? create new item_node/
+              /create relations *jika sudah ada maka ditambah ke ...*/
+              /mungkin didalam relasi ditambahkan juga atribut dia sudah pernah membeli berapa kali/
+          /else/
+            /check is item_node exist?/
+              /if not_exist? create new item_node/
+              /else/
+              /create relations *jika sudah ada maka ditambah ke ...*/
+              /mungkin didalam relasi ditambahkan juga atribut dia sudah pernah membeli berapa kali/
+	end
 
+
+	def generate
+		/executing query with selected algorhytm/
+		//
+		
 	end
 end
