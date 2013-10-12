5503e4cbc5e64ce5eaa9de6001e92689e85b2bf8
dvdekow <dvdekow@gmail.com>
2013-10-12 10:07:01 +0700

    simulating node

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -25,31 +25,31 @@ class ResultsController < ApplicationController
 	def makeRelations
     	/check is user_node exist?/
 		  /if not_exist? create new user_node/
-		  	user_node = @neo.create_node("user_id" => params[:user_id])
+		  	@user_node = @neo.create_node("user_id" => params[:user_id])
            /check jika telah melihat item lain
              jika ada maka buat node melihat yang lain
            /
 			/check is item_node exist?/
               /if not_exist? create new item_node/
-              item_node = @neo.create_node("product_id" => params[:product_id])
+              @item_node = @neo.create_node("product_id" => params[:product_id])
               /create relations *jika sudah ada maka ditambah ke ...*/
               /mungkin didalam relasi ditambahkan juga atribut dia sudah pernah membeli berapa kali/
 
 
            /jika ada node item belum pernah dibuat/
-            item_node = @neo.create_node("item_id" => params[:item_id])
+            @item_node = @neo.create_node("item_id" => params[:item_id])
            /jika sudah ada maka item_node = find node by item id/
            /create relations/
-            view_relation = @neo.create_relationship("view", user_node, item_node)
+            @view_relation = @neo.create_relationship("view", user_node, item_node)
 
 
            /
            check jika telah membeli barang lain
              jika ada maka buat node beli yang lain
            /
-            item_buy_node = @neo.create_node("item_id" => params[:item_id])
+            @item_buy_node = @neo.create_node("item_id" => params[:item_id])
            /jika sudah ada maka item_buy_node = find node ny item id/
-            buy_relation = @neo.create_relationship("buy", user_node, item_node)
+            @buy_relation = @neo.create_relationship("buy", user_node, item_node)
            /
              check jika pernah memberi rating
              jika pernah memberi rating pada barang
@@ -59,7 +59,7 @@ class ResultsController < ApplicationController
              check apakah ia penjual barang
              jika pernah check rekomendasinya
 		   /
-		    user_recommendation_node = @neo.set_node_properties(user_node, {"recommendation" => params[:recommendation]}) 
+		    @user_recommendation_node = @neo.set_node_properties(user_node, {"recommendation" => params[:recommendation]}) 
 
 
           /else/
@@ -74,7 +74,7 @@ class ResultsController < ApplicationController
 	def generate
 		/executing query with selected algorhytm/
 		//
-		
+		@result = @neo.execute_query("")
 	end
 
 end
